f280a1197f5185fcf111a72ba27c216ddcc60377
andela-amaborukoje <adebayo@andela.co>
2015-01-09 11:27:09 +0100

    allow cross origin implementation

--- app.js
+++ app.js
@@ -2,20 +2,35 @@ var express = require('express');
 var routes = require('./route/route');
 var app = express();
 
-var allowCrossOrigin = function (request, response, next){
-response.header('Access-Control-Allow-Origin', '*');
-response.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
-// ******(The following is used when Authorization is required)****
-//response.header('Access-Control-Allow-Headers', 'Content-Type Authorization Content-Length, X-Requested-With ');
-//intercepts Options method
-/* if ('OPTIONS'=== req.method){
-  response.send(200);
-  }
-  else  {
-    next();
-  } */
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
 };
-app.use(allowCrossOrigin);
+
+app.use(allowCrossDomain);
+// var allowCrossOrigin = function (request, response, next){
+// response.header('Access-Control-Allow-Origin', '*');
+// response.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
+// // ******(The following is used when Authorization is required)****
+// //response.header('Access-Control-Allow-Headers', 'Content-Type Authorization Content-Length, X-Requested-With ');
+// //intercepts Options method
+// /* if ('OPTIONS'=== req.method){
+//   response.send(200);
+//   }
+//   else  {
+//     next();
+//   } */
+// };
+// app.use(allowCrossOrigin);
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
